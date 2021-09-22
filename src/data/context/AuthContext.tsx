2b29a3e9a66b69a5ce8b2d97f101792afc07c856
Elias Braga <eliasjrb.eb@gmail.com>
2021-09-21 23:56:01 -0400

    autenticação com google

--- src/data/context/AuthContext.tsx
+++ src/data/context/AuthContext.tsx
@@ -1,2 +1,50 @@
+import route from 'next/router'
+import { createContext, useState } from "react"
 import firebase from "../../firebase/config"
+import Usuario from '../../model/Usuario'
 
+interface AuthContextProps{
+    usuario?: Usuario
+    loginGoogle?: () => Promise<void>
+}
+
+const AuthContext = createContext<AuthContextProps>({})
+
+async function usuarioNormalizado(usuarioFirebase: firebase.User): Promise<Usuario>
+{
+    const token = await usuarioFirebase.getIdToken()
+    return {
+        uid: usuarioFirebase.uid,
+        nome: usuarioFirebase.displayName,
+        email: usuarioFirebase.email,
+        token,
+        provedor: usuarioFirebase.providerData[0].providerId,
+        imagemUrl: usuarioFirebase.photoURL
+    }
+}
+
+export function AuthProvider(props) {
+    const [usuario, setUsuario] = useState<Usuario>(null)
+
+    async function loginGoogle(){
+        const resp = await firebase.auth().signInWithPopup(
+            new firebase.auth.GoogleAuthProvider()
+        )
+        if(resp.user?.email){
+            const usuario = await usuarioNormalizado(resp.user)
+            setUsuario(usuario)
+            route.push("/")
+        }
+    }
+
+    return (
+        <AuthContext.Provider value={{
+            usuario,
+            loginGoogle
+        }}>
+            {props.children}
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthContext
--- src/pages/autenticacao.js
+++ src/pages/autenticacao.js
@@ -1,8 +1,12 @@
 import { useState } from 'react'
 import AuthInput from '../components/auth/AuthInput'
 import { IconeAtencao } from '../components/icons'
+import useAuth from '../data/hook/useAuth'
 
 export default function Autenticacao() {
+
+    const { usuario, loginGoogle} = useAuth()
+
     const [erro, setErro] = useState(null)
     const [modo, setModo] = useState('login')
     const [email, setEmail] = useState('')
@@ -72,7 +76,7 @@ export default function Autenticacao() {
 
                 <hr className={`my-6 border-gray-300 w-full`} />
 
-                <button onClick={submeter} className={`
+                <button onClick={loginGoogle} className={`
                 w-full bg-red-500 
                 hover:bg-red-400
                 text-white rounded-lg px-4 py-3
