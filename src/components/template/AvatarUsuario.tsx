958fd4cc71a9c47f3d0c5fe9b4dea807d490f01c
Elias Braga <eliasjrb.eb@gmail.com>
2021-09-22 22:02:39 -0400

    ajuste nome avatar

--- src/components/template/AvatarUsuario.tsx
+++ src/components/template/AvatarUsuario.tsx
@@ -5,16 +5,19 @@ interface AvatarUsuarioProps {
     className?: string
 }
 
-export default function AvatarUsuario(props: AvatarUsuarioProps){
+export default function AvatarUsuario(props: AvatarUsuarioProps) {
     const { usuario } = useAuth()
 
     return (
-        <Link href="/perfil">
-            <img 
-                src={usuario?.imagemUrl ?? '/images/avatar.svg'} 
-                alt="Avatar do Usuário" 
-                className={` h-10 w-10 rounded-full cursor-pointer ${props.className}`}
-            />
-        </Link>
+        <div  className={`flex justify-center items-center text-gray-800 dark:text-white`}>
+            <span className={`flex ml-3`}>{usuario?.nome}</span>
+            <Link href="/perfil">
+                <img
+                    src={usuario?.imagemUrl ?? '/images/avatar.svg'}
+                    alt="Avatar do Usuário"
+                    className={` h-10 w-10 mr-2 rounded-full cursor-pointer ${props.className}`}
+                />
+            </Link>
+        </div>
     )
 }
